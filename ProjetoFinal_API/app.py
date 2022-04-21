94f770632fe18dfde2b6a5dcfa2253ed2b57e94d
Laiane <laiane.rabello@gmail.com>
2022-04-21 22:08:28 +0100

    Atualizações

--- ProjetoFinal_API/app.py
+++ ProjetoFinal_API/app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, current_user, logout_user
-
+from datetime import date
 
 
 # main = Blueprint('main', __name__)
